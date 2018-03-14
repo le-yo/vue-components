92bddc1eac1d203990e7d0b800ff24e5d07d8041
Le-Yo <le-yo@le-yo.com>
2018-03-14 14:44:36 +0300

    working components

---
+++ src/ServerStatus.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+        <p>Server Status: {{status}}</p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+<script>
+    export default {
+        data : function (){
+            return {
+                status: 'critical'
+            }
+        },
+        methods : {
+            changeStatus(){
+                if(this.status == 'Normal'){
+                    this.status = 'Critical';
+                }else{
+
+                    this.status = 'Normal'
+                }
+            }
+        }
+    }
+</script>
